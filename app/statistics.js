a3fd0ff67b1377aeba96ae99eb4c475c99a71b40
Sakthivel Karthikeyan <32382572+myselfsakthivel@users.noreply.github.com>
2022-10-20 14:48:02 +0530

    Initial logic implementation

--- app/statistics.js
+++ app/statistics.js
@@ -1,5 +1,13 @@
-module.exports=function getStatistics(numbers){
-    
+module.exports = function getStatistics(numbers) {
     //implement the computation of statistics here
+    var dataToReturn = {min: 0, max: 0, average: 0};
+    var sum = 0;
+    dataToReturn.min = Math.min(...numbers);
+    dataToReturn.max = Math.max(...numbers);
+    
+    sum = numbers.reduce((partSum, a) => partSum + a, 0);
+    dataToReturn.average = sum/numbers.length;
+
+    return dataToReturn;
 }
 
